03273dab36584ee4d207e59adb157835d8624d43
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-01-25 18:43:00 +0700

    EC-15 || Add categories management features; implement CategoriesPage, CategoryClient, and API routes || HauzanDev

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import React, { useState } from "react";
+import { CategoryColumn } from "./columns";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modals";
+
+interface CellActionProps {
+  data: CategoryColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onCopyId = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Id copied to clipboard");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+      router.refresh();
+      router.push(`/${params.storeId}/categories`);
+      toast.success("Category berhasil dihapus");
+    } catch (error) {
+      toast.error("Gagal menghapus store");
+    } finally {
+      setOpen(false);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={() => onDelete()}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <span className="sr-only">Open Menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopyId(data.id)}>
+            <Copy className="mr-2 h-4 w-4" />
+            Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/${params.storeId}/categories/${data.id}`)
+            }
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/Heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { CategoryColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex justify-between items-center ">
+        <Heading
+          title={`Category (${data.length})`}
+          description="Atur Category Toko Anda"
+        />
+        <Button
+          onClick={() => {
+            router.push(`/${params.storeId}/categories/new`);
+          }}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API untuk Categories" />
+      <Separator />
+      <ApiList namaIndikator="categories" idIndikator="categoryId" />
+    </>
+  );
+};
