ae0459c0ac0cf312f8e4b9ada2c9ab3f523f183e
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-01-27 11:28:10 +0700

    EC-26 || Refactor Prisma client initialization to use 'let' for global declaration and improve readability || HauzanDev

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  let prisma: PrismaClient | undefined;
 }
 
-const db = global.globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
 
 export default db;
